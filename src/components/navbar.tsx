e3da59b08ae1d7a6393bafe52ff1d54b41ae001f
MaadhyamRana <864mmr@gmail.com>
2024-07-12 11:38:12 +0300

    Initial Commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function navbar({}: Props) {
+  return (
+    <div className="shadow-sm sticky top-0 left-0 z-50 bg-white">navbar</div>
+  )
+}
